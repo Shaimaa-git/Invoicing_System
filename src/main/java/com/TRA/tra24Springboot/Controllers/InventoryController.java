1d15e5a5710e80822e371c3232b6aa7f7d9b0f28
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-05-07 10:43:41 +0400

    create two functions recieve stock  and report

--- src/main/java/com/TRA/tra24Springboot/Controllers/InventoryController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InventoryController.java
@@ -1,7 +1,37 @@
 package com.TRA.tra24Springboot.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.Inventory;
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.Supplier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
+@RequestMapping("/inventory")
 public class InventoryController {
+    private Inventory globalInventoryItem = new Inventory();
+
+    @PostMapping("receive")
+    public Inventory receiveStock(@RequestBody Inventory inventoryItem) {
+        inventoryItem.setId(1);
+        inventoryItem.setCreatedDate(new Date());
+        inventoryItem.setUpdatedDate(new Date());
+        inventoryItem.setPhoneNumber("91767067");
+        inventoryItem.setClosingHours("10:00 PM");
+        inventoryItem.setSupplier("chine company");
+        inventoryItem.setManager("SHaimaa");
+        inventoryItem.setLocation("Nizwa");
+        inventoryItem.setIsActive(Boolean.TRUE);
+        inventoryItem.setOpeningHours("8:00AM");
+        globalInventoryItem = inventoryItem;
+        return inventoryItem;
+    }
+    @GetMapping("report")
+    public  Inventory reportInventory(){
+        return  globalInventoryItem;
+    }
+
 }
--- src/main/java/com/TRA/tra24Springboot/Controllers/SupplierController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/SupplierController.java
@@ -1,4 +1,14 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.Models.Supplier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplier")
 public class SupplierController {
+
+    Supplier supplierDetails=new Supplier();
+
+
 }
