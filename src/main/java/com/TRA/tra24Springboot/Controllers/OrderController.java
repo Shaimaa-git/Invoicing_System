f34b6c11bee3d1955e5e0e0b03522129eaae2897
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-05-07 20:32:10 +0400

    create functions of creating, updating, and canceling order

--- src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
@@ -1,10 +1,38 @@
 package com.TRA.tra24Springboot.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.TRA.tra24Springboot.Models.Order;
+import com.TRA.tra24Springboot.Models.OrderStatus;
+import com.TRA.tra24Springboot.Models.PaymentStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+    @PostMapping("create")
+    public Order createOrder(@RequestBody Order order) {
 
+        order.setOrderDate(new Date());
+        order.setStatus(OrderStatus.IN_PROGRESS);
+        order.setPaymentStatus(PaymentStatus.PAID);
+        return order;
+    }
+    @PutMapping("/update")
+    public Order updateOrder(@RequestBody Order order) {
+        order.setOrderDate(new Date());
+        return order;
+    }
+    @PostMapping("/cancel/{orderId}")
+    public String cancelOrder(@PathVariable("orderId") String orderId,Order order) {
+        if (order != null && order.getStatus() == OrderStatus.IN_PROGRESS) {
+            order.setStatus(OrderStatus.CANCELED);
+            if (order.getPaymentStatus() == PaymentStatus.PAID) {
+                order.setPaymentStatus(PaymentStatus.REJECTED);
+            }
+            return "Order with ID " + orderId + " has been canceled.";
+        } else {
+            return "Unable to cancel order. Order may not exist or may not be cancelable.";
+        }
+    }
 }
