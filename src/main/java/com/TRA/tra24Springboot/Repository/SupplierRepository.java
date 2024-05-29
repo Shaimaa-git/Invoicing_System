49bdfa8eb5f127cb07ebb302ec8dfba9d50388ff
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-05-29 12:05:00 +0400

    edit query of supplier id

--- src/main/java/com/TRA/tra24Springboot/Repository/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/SupplierRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.query.Param;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
     @Query("SELECT s from Supplier s WHERE s.id =:supplierId")
-    Supplier getSupplierById(@Param("productId") Integer supplierId );
+    Supplier getSupplierById(@Param("supplierId") Integer supplierId );
 }
