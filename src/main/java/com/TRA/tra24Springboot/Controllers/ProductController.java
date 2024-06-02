1cdfd136eaf0a006c64ea74a1aaf3ec3fefe4956
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-06-02 09:44:57 +0400

    add exception for handling error

--- src/main/java/com/TRA/tra24Springboot/Controllers/ProductController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.TRA.tra24Springboot.Controllers;
 import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -19,12 +22,22 @@ public class ProductController {
         return productService.saveProduct(product);
     }
     @PostMapping("deleteById")
-    public String deleteProductById(@RequestParam Integer productId){
-        return productService.deleteProductById(productId);
+    public <T> ResponseEntity<T> deleteProductById(@RequestParam Integer id) throws Exception {
+        try {
+            String result = productService.deleteProductById(id);
+            return (ResponseEntity<T>) new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return (ResponseEntity<T>) new ResponseEntity<>("Deleting failed! " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @PostMapping("deleteByName")
-    public String deleteProductByName(@RequestParam String productName){
-        return productService.deleteProduct(productName);
+    public <T> ResponseEntity<T> deleteProductByName(@RequestParam String productName) throws Exception{
+        try {
+            String result = productService.deleteProductByName(productName);
+            return (ResponseEntity<T>) new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e){
+            return (ResponseEntity<T>) new ResponseEntity<>("Deleting failed! " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("update")
