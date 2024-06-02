a590e92a5ab50d504391f3343e34de0b5e485242
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-06-02 09:52:54 +0400

    add exception to write-off  for inventory class

--- src/main/java/com/TRA/tra24Springboot/Controllers/InventoryController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InventoryController.java
@@ -2,6 +2,8 @@ package com.TRA.tra24Springboot.Controllers;
 import com.TRA.tra24Springboot.Models.Inventory;
 import com.TRA.tra24Springboot.Services.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -25,7 +27,12 @@ public class InventoryController {
         return inventoryService.saveReturnStock(inventoryItem);
     }
     @PostMapping("write-off")
-    public String writeOffInventory(@RequestBody Integer inventoryId) {
-        return inventoryService.deleteInventoryById(inventoryId);
+    public <T> ResponseEntity<T> deleteInventoryById(@RequestParam Integer inventoryId) throws Exception {
+        try {
+            String result =inventoryService.deleteInventoryById(inventoryId);
+            return (ResponseEntity<T>) new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return (ResponseEntity<T>) new ResponseEntity<>("Deleting failed! " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
