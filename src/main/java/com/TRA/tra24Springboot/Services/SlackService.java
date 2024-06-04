dc87d3f199b4857db22ffa075dadc5d952da314e
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-06-04 11:00:39 +0400

    testing Cron

--- src/main/java/com/TRA/tra24Springboot/Services/SlackService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/SlackService.java
@@ -5,6 +5,7 @@ import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +14,29 @@ public class SlackService {
 
     @Value("${slack.token.integration}")
     private String slackToken;
-
+    //@Scheduled(cron = "* 0/1 * * * *") //testing cron
+    public  void  sendSchduledMessge(){
+        String channel = "#shaimaa";
+        String message = "Testing CRON";
+        sendMessageCRON(channel,message);
+    }
+    public void sendMessageCRON( String channel, String message) {
+        Slack slack = Slack.getInstance();
+        ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                .channel(channel)
+                .text(message)
+                .build();
+        try {
+            ChatPostMessageResponse response = slack.methods(slackToken).chatPostMessage(request);
+            if (response.isOk()) {
+                System.out.println("Message sent successfully to Slack!");
+            } else {
+                System.out.println("Failed to send message to Slack: " + response.getError());
+            }
+        } catch (Exception e) {
+            System.out.println("Error sending message to Slack: " + e.getMessage());
+        }
+    }
     public void sendMessage( String channel, String message) {
         Slack slack = Slack.getInstance();
 
