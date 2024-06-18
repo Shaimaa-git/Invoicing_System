1d49ec5c1a90c3fa0d03e475bc6f5f9ca81d68a2
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-06-19 00:12:05 +0400

    add queries to supplier service

--- src/main/java/com/TRA/tra24Springboot/Repository/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/SupplierRepository.java
@@ -1,12 +1,35 @@
 package com.TRA.tra24Springboot.Repository;
 
+import com.TRA.tra24Springboot.Models.PaymentType;
 import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
     @Query("SELECT s from Supplier s WHERE s.id =:supplierId")
     Supplier getSupplierById(@Param("supplierId") Integer supplierId );
+    //Query to get supplier by company name
+    @Query("SELECT s FROM Supplier s WHERE s.companyName =:companyName")
+    List<Supplier> getSupplierByCompanyName(@Param("companyName") String companyName);
+    //Query to get suppliers by country
+    @Query("SELECT s FROM Supplier s WHERE s.country =:country")
+    List<Supplier> getSupplierByCountry(@Param("country") String country);
+
+    @Query("SELECT s FROM Supplier s WHERE s.minimumOrderQuantity =:minimumOrderQuantity")
+    List<Supplier> getSupplierByMinimumOrderQuantity(@Param("minimumOrderQuantity") Integer minimumOrderQuantity);
+
+    @Query("SELECT s FROM Supplier s WHERE s.isActive =:isActive")
+    List<Supplier> findBySupplierByIsActive( @Param("isActive") Boolean isActive );
+
+
+    @Query("SELECT s FROM Supplier s WHERE s.shippingMethods =:shippingMethods")
+    List<Supplier> findBySupplierByShippingMethods(@Param("shippingMethods") String shippingMethods );
+
+    //Query to get supplier by payment method
+    @Query("SELECT s FROM Supplier s WHERE s.paymentMethods =:paymentMethods")
+    List<Supplier> getSupplierByPaymentMethod(@Param("paymentMethods") PaymentType paymentMethods);
 }
