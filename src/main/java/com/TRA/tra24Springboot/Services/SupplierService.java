beb5641c815174698ac72b35494e0c4f3eea2ef8
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-06-19 00:12:34 +0400

    add functions to get supplier by any attribute

--- src/main/java/com/TRA/tra24Springboot/Services/SupplierService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/SupplierService.java
@@ -1,6 +1,7 @@
 package com.TRA.tra24Springboot.Services;
 
 import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.PaymentType;
 import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Models.Supplier;
 import com.TRA.tra24Springboot.Repository.SupplierRepository;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class SupplierService {
@@ -57,6 +59,74 @@ public class SupplierService {
 
         return supplierRepository.save(supplier);
     }
+    public List<Supplier> getSupplierByCompanyName (String companyName) throws  Exception{
+        try {
+            List<Supplier> suppliers = supplierRepository.getSupplierByCompanyName(companyName);
+            if (suppliers.isEmpty()) {
+                throw new Exception("No Supplier found with Company Name " + companyName);
+            }
+            return suppliers;
+        }catch (Exception e) {
+            throw new Exception("Failed to retrieve suppliers by Company name: " + e.getMessage(), e);}
+    }
+    public List<Supplier> getSupplierByCountry (String countr) throws  Exception{
+        try {
+            List<Supplier> suppliers = supplierRepository.getSupplierByCountry(countr);
+            if (suppliers.isEmpty()) {
+                throw new Exception("No Supplier found with Country  Name " + countr);
+            }
+            return suppliers;
+        }catch (Exception e){
+            throw new Exception("Failed to retrieve suppliers by Country  Name"+e.getMessage());
+        }
 
+    }
+
+    public List<Supplier> getSupplierByMinimumOrderQuantity (Integer minimumOrderQuantity) throws  Exception{
+        try{
+            List<Supplier> suppliers = supplierRepository.getSupplierByMinimumOrderQuantity(minimumOrderQuantity);
+            if (suppliers.isEmpty()){
+                throw new Exception("No Suppliers found with Minimum Order Quantity"+minimumOrderQuantity);
+            }
+            return suppliers;
+        }catch (Exception e){
+            throw  new Exception("Failed to retrieve suppliers By Minimum Order Quantity"+e.getMessage());
+        }
+    }
+
+    public List<Supplier> getSupplierByIsActive (Boolean isActive)throws  Exception{
+        try {
+            List<Supplier> suppliers = supplierRepository.findBySupplierByIsActive(isActive);
+            if(suppliers.isEmpty()){
+                throw  new Exception("No suppliers found with isActive: " + isActive);
+            }
+            return  suppliers;
+        }catch (Exception e){
+            throw new Exception("Failed to retrieve suppliers With isActive : " + e.getMessage(), e);
+        }
+    }
+
+    public List<Supplier> findBySupplierByShippingMethods (String shippingMethods) throws  Exception{
+        try {
+            List<Supplier> suppliers = supplierRepository.findBySupplierByShippingMethods(shippingMethods);
+            if(suppliers.isEmpty()){
+                throw  new Exception("No suppliers found with  this shipping Method: " + shippingMethods);
+            }
+            return  suppliers;
+        }catch (Exception e){
+            throw new Exception("Failed to retrieve suppliers shipping Method : " + e.getMessage(), e);
+        }
+    }
+    public List<Supplier> getSupplierByPaymentMethod (PaymentType paymentMethods) throws  Exception {
+        try {
+            List<Supplier> suppliers = supplierRepository.getSupplierByPaymentMethod(paymentMethods);
+            if (suppliers.isEmpty()) {
+                throw new Exception("No suppliers found with this Payment method " + paymentMethods);
+            }
+            return suppliers;
+        } catch (Exception e) {
+            throw new Exception("Failed to retrieve suppliers Payment method : " + e.getMessage(), e);
+        }
+    }
 
 }
