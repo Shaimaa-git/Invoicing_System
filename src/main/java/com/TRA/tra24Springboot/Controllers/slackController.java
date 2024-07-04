52980aa5be4d453abe07549adee91cc0b5fecb78
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-07-04 09:11:53 +0400

    remove extra lines

--- src/main/java/com/TRA/tra24Springboot/Controllers/slackController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/slackController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/slack")
+
 public class slackController {
     @Autowired
     SlackService slackService;
