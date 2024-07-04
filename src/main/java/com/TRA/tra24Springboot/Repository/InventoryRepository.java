bc930b984dc7c7e4dab4025b2d011d03333a64e6
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-07-04 09:09:13 +0400

    delete unused lines

--- src/main/java/com/TRA/tra24Springboot/Repository/InventoryRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/InventoryRepository.java
@@ -25,4 +25,6 @@ public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
     //Query to get inventory by admin name
     @Query("SELECT i FROM Inventory i WHERE i.manager =:manager")
     List<Inventory> getInventoryByManagerName(@Param("manager") String manager);
+
+
 }
