981d75ff236b903b3c0dc188044cb68a4344b029
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-07-04 10:00:04 +0400

    add send reminder fun to set the reminder of due date for invoice

--- src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
@@ -4,9 +4,13 @@ import com.TRA.tra24Springboot.Models.Invoice;
 import com.TRA.tra24Springboot.Services.InvoiceService;
 import com.TRA.tra24Springboot.Services.SlackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/invoice")
@@ -20,4 +24,13 @@ public class InvoiceController {
       slackService.sendMessage("shaimaa", "Invoice created successfully");
       return invoiceService.createInvoice(invoice);
   }
+    @GetMapping("/DueDate")
+    @Scheduled(cron = "0 0 9 * * ?")  // every day at 9 AM
+    public void sendDueReminders() {
+        List<Invoice> dueInvoices = invoiceService.findInvoicesDueInNextDays(7);  // Remind 7 days in advance
+        for (Invoice invoice : dueInvoices) {
+            String message = "Reminder: Invoice " + invoice.getId() + " is due on " + invoice.getDueDate();
+            slackService.sendMessage("shaimaa", message);
+        }
+    }
 }
