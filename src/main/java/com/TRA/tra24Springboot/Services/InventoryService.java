d907e4ffa1797cdd535cc8aaf476d933499d72e6
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-07-17 17:52:15 +0400

    update inventory service

--- src/main/java/com/TRA/tra24Springboot/Services/InventoryService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/InventoryService.java
@@ -1,33 +1,89 @@
 package com.TRA.tra24Springboot.Services;
 
-import com.TRA.tra24Springboot.Models.Inventory;
-import com.TRA.tra24Springboot.Models.Order;
-import com.TRA.tra24Springboot.Models.Product;
-import com.TRA.tra24Springboot.Repository.InventoryRepository;
+import com.TRA.tra24Springboot.DTO.InventoryDTO;
+import com.TRA.tra24Springboot.Models.*;
+import com.TRA.tra24Springboot.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class InventoryService {
     @Autowired
     InventoryRepository inventoryRepository;
-    public Inventory saveReceiveStock(@RequestBody Inventory inventoryItem) {
-        inventoryItem.setId(1);
-        inventoryItem.setCreatedDate(new Date());
-        inventoryItem.setUpdatedDate(new Date());
-        inventoryItem.setPhoneNumber("91767067");
-        inventoryItem.setClosingHours("10:00 PM");
-        inventoryItem.setSupplier("chine company");
-        inventoryItem.setManager("SHaimaa");
-        inventoryItem.setLocation("Nizwa");
-        inventoryItem.setIsActive(Boolean.TRUE);
-        inventoryItem.setOpeningHours("8:00AM");
-        return inventoryRepository.save(inventoryItem);
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    SupplierRepository supplierRepository;
+
+    @Autowired
+    ProductsDetailsRepository productDetailsRepository;
+    @Autowired
+     ProductDetails productDetails;
+    InventoryDTO inventoryDTO;
+    @Autowired
+    Supplier supplier;
+    @Autowired
+    Product product;
+    public Inventory saveReceiveStock(@RequestBody Inventory inventory) {
+
+        productDetails.setName("Laptop");
+        productDetails.setColor("Black");
+        productDetails.setPrice(350d);
+        productDetails.setCountryOfOrigin("USA");
+        productDetails.setCreatedDate(new Date());
+        productDetails = productDetailsRepository.save(productDetails);
+
+        product.setProductDetails(productDetails);
+        product.setSku(UUID.randomUUID());
+        product.setQuantity(100);
+        product.setIsActive(Boolean.TRUE);
+        product.setCreatedDate(new Date());
+
+        product = productRepository.save(product);
+
+        Order order = new Order();
+        order.setProductsOrdered(Arrays.asList(product)); //setting the products lists
+        order.setCategoryName("Electronics");
+        order.setCreatedDate(new Date());
+        order.setOrderDate(new Date());
+        order.setStatus(OrderStatus.IN_PROGRESS);
+        order.setPaymentStatus(PaymentStatus.PAID);
+        order.setPaymentType(PaymentType.BANK_TRANSFER);
+        order.setDueDate(new Date());
+        order = orderRepository.save(order);
+
+        supplier.setCompanyName("Dell");
+        supplier.setOrders(Arrays.asList(order));
+        supplier.setCountry("USA");
+        //supplier.setContactDetails(contactDetails);
+        supplier.setMinimumOrderQuantity(2);
+        supplier.setCreatedDate(new Date());
+        supplier.setIsActive(Boolean.TRUE);
+        supplier = supplierRepository.save(supplier);
+
+        inventory.setProducts(Arrays.asList(product));
+        inventory.setLocation("Salalah");
+        inventory.setManager("Noura");
+        inventory.setPhoneNumber("12345778");
+        inventory.setSupplier(Arrays.asList(supplier));
+        inventory.setOpeningHours("8 AM");
+        inventory.setClosingHours("8 PM");
+        inventory.setWorkers(Arrays.asList("Jack", "Andrew", "Sam"));
+        inventory.setCreatedDate(new Date());
+        inventory.setIsActive(Boolean.TRUE);
+
+        return inventoryRepository.save(inventory);
     }
     public Inventory saveReturnStock(@RequestBody Inventory inventoryItem) {
 
