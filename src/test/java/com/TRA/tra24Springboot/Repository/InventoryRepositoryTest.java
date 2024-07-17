370d7e79a074adf04d842c8bee4f6d25e98a2ba1
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-07-17 17:53:34 +0400

    update inventory repository test

--- src/test/java/com/TRA/tra24Springboot/Repository/InventoryRepositoryTest.java
+++ src/test/java/com/TRA/tra24Springboot/Repository/InventoryRepositoryTest.java
@@ -1,8 +1,8 @@
 package com.TRA.tra24Springboot.Repository;
 
 
-import com.TRA.tra24Springboot.Models.BaseEntity;
-import com.TRA.tra24Springboot.Models.Inventory;
+import com.TRA.tra24Springboot.Models.*;
+import com.TRA.tra24Springboot.Utils.DateHelperUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -20,46 +23,110 @@ import java.util.List;
 
 class InventoryRepositoryTest extends BaseEntity {
     @Autowired
-    InventoryRepository inventoryRepository;
-
+    private InventoryRepository inventoryRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ProductsDetailsRepository productDetailsRepository;
+    @Autowired
+    private SupplierRepository supplierRepository;
+    @Autowired
+    private ContactDetailsRepository contactDetailsRepository;
+    private UUID sku;
+    private Date date;
     @BeforeEach
-    void setUp() {
-        Inventory inventory1 =Inventory.builder()
-                .location("Warehouse A")
-                .manager("Manager1")
+    void setupInventory(){
+        ProductDetails productDetails = ProductDetails.builder()
+                .name("Screen")
+                .color("Black")
+                .countryOfOrigin("Japan")
+                .price(200.0)
+                .size("24 inches")
                 .build();
+        productDetailsRepository.save(productDetails);
 
-        Inventory inventory2 = Inventory.builder()
-                .location("Warehouse B")
-                .manager("Manager2")
+        sku = UUID.randomUUID();
+
+        Product product = Product.builder()
+                .productDetails(productDetails)
+                .category("Electronics")
+                .quantity(50)
+                .sku(sku)
                 .build();
+        product.setIsActive(Boolean.TRUE);
+        productRepository.save(product);
 
-        inventoryRepository.save(inventory1);
-        inventoryRepository.save(inventory2);
-    }
-    @Test
-    public void getByInventoryId() {
-        Inventory inventory = inventoryRepository.getByInventoryId(1);
-        assertThat(inventory).isNotNull();
-        assertThat(inventory.getId()).isEqualTo(1);
-    }
-    @Test
-    public void getInventoryByAvailability() {
-        List<Inventory> inventories = inventoryRepository.getInventoryByAvailability(true);
-        assertThat(inventories).hasSize(1);
-        assertThat(inventories.get(0).getIsActive()).isEqualTo(true);
-    }
-    @Test
-    public void getInventoryByLocation() {
-        List<Inventory> inventories = inventoryRepository.getInventoryByLocation("Warehouse B");
-        assertThat(inventories).hasSize(1);
-        assertThat(inventories.get(0).getLocation()).isEqualTo("Warehouse B");
-    }
-    @Test
-    public void getInventoryByManagerName() {
-        List<Inventory> inventories = inventoryRepository.getInventoryByManagerName("Manager1");
-        assertThat(inventories).hasSize(1);
-        assertThat(inventories.get(0).getManager()).isEqualTo("Manager1");
+        date = new Date();
+        Order order = Order.builder()
+                .productsOrdered(List.of(product))
+                .categoryName("Electronics")
+                .paymentType(PaymentType.BANK_TRANSFER)
+                .status(OrderStatus.COMPLETED)
+                .paymentStatus(PaymentStatus.PAID)
+                .orderDate(new Date())
+                .dueDate(DateHelperUtils.addDays(date, 7))
+                .build();
+        orderRepository.save(order);
+
+        ContactDetails contactDetails = ContactDetails.builder()
+                .email("supplier@info.com")
+                .address("Tokyo, Chiyoda City, Marunouchi, 1 Chome−1")
+                .phoneNumber("+81 3-1234-5678")
+                .postalCode("〒100-0005")
+                .faxNumber("+81 3-1234-5678")
+                .build();
+        contactDetailsRepository.save(contactDetails);
+
+        Supplier supplier = Supplier.builder()
+                .companyName("Sharp Corporation")
+                .country("Japan")
+                .expectedProducts(List.of(product))
+                .orders(List.of(order))
+                .contactDetails(contactDetails)
+                .paymentMethods(PaymentType.BANK_TRANSFER)
+                .shippingMethods("Freight Shipping")
+                .minimumOrderQuantity(1)
+                .nextDeliveryTime(DateHelperUtils.addDays(date, 20))
+                .build();
+        supplierRepository.save(supplier);
+
+        Inventory inventory = Inventory.builder()
+                .products(List.of(product))
+                .supplier(List.of(supplier))
+                .manager("shaimaa")
+                .location("Muscat")
+                .phoneNumber("12345678")
+                .workers(Arrays.asList("Tayba", "Noura", "Balqees", "Shaimaa"))
+                .openingHours("9 AM")
+                .closingHours("9 PM")
+                .build();
+        inventory.setIsActive(Boolean.TRUE);
+        inventoryRepository.save(inventory);
     }
 
+    @Test
+    void getInventoryByAvailability() {
+        List<Inventory> inventoryAvailability = inventoryRepository.getInventoryByAvailability(Boolean.TRUE);
+        assertThat(inventoryAvailability).isNotNull();
+        assertThat(inventoryAvailability.size()).isEqualTo(1);
+        assertThat(inventoryAvailability.get(0).getIsActive()).isEqualTo(Boolean.TRUE);
+    }
+
+    @Test
+    void getInventoryByLocation() {
+        List<Inventory> inventoryLocations = inventoryRepository.getInventoryByLocation("Muscat");
+        assertThat(inventoryLocations).isNotNull();
+        assertThat(inventoryLocations.size()).isEqualTo(1);
+        assertThat(inventoryLocations.get(0).getLocation()).isEqualTo("Muscat");
+    }
+
+    @Test
+    void getInventoryByAdminName() {
+        List<Inventory> inventoryAdmins = inventoryRepository.getInventoryByManagerName("shaimaa");
+        assertThat(inventoryAdmins).isNotNull();
+        assertThat(inventoryAdmins.size()).isEqualTo(1);
+        assertThat(inventoryAdmins.get(0).getManager()).isEqualTo("shaimaa");
+    }
 }
